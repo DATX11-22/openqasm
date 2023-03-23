0bc632d951c07e02c50f471b10f1832e0f7a736b
edvinsternvik <e.sternvik@gmail.com>
2023-03-23 15:49:33 +0100

    Implement support for include statements
    If an include statement is found then the included file is going to be
    tokenized and then spliced into the previous token vector.

--- openqasm/src/main.rs
+++ openqasm/src/main.rs
@@ -26,12 +26,7 @@ pub fn parse_openqasm(file_path: &Path) -> Result<OpenQASMProgram, OpenQASMError
     let mut lexer = Lexer::new();
     lexer_rules::add_open_qasm_rules(&mut lexer);
 
-    let file_str = read_to_string(file_path).map_err(|_| OpenQASMError::FileError)?;
-    let file_chars = file_str.bytes().map(|b| b as char).collect();
-
-    let tokens = lexer
-        .parse(file_chars)
-        .map_err(|_| OpenQASMError::TokenError)?;
+    let tokens = read_file_tokens(&lexer, file_path)?;
 
     let t_vec: Vec<TokenMatch> = tokens
         .iter()
@@ -41,6 +36,34 @@ pub fn parse_openqasm(file_path: &Path) -> Result<OpenQASMProgram, OpenQASMError
     let ast = parse::<MainProgram, Token>(&t_vec).map_err(|_| OpenQASMError::SyntaxError)?;
 
     OpenQASMProgram::from_ast(&ast).map_err(|e| OpenQASMError::SemanticError(e))
+}
+
+fn read_file_tokens(
+    lexer: &Lexer<Token>,
+    file_path: &Path,
+) -> Result<Vec<(Token, Vec<char>)>, OpenQASMError> {
+    let file_str = read_to_string(file_path).map_err(|_| OpenQASMError::FileError)?;
+    let file_chars = file_str.bytes().map(|b| b as char).collect();
+
+    let mut tokens = lexer
+        .parse(file_chars)
+        .map_err(|_| OpenQASMError::TokenError)?;
+
+    for i in 0..tokens.len() {
+        if i + 2 < tokens.len()
+            && tokens[i + 0].0 == Token::Include
+            && tokens[i + 1].0 == Token::Str
+            && tokens[i + 2].0 == Token::Semicolon
+        {
+            let other_file_path = tokens[i + 1].1.clone().into_iter().collect::<String>();
+            let other_file_path = &other_file_path[1..other_file_path.len() - 1];
+            let other_tokens = read_file_tokens(lexer, Path::new(other_file_path))?;
+
+            tokens.splice(i..i+3, other_tokens);
+        }
+    }
+
+    Ok(tokens)
 }
 
 fn main() {
